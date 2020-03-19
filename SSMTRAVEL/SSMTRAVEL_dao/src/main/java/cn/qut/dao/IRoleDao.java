bef0ee1e4513b382daabe33f73420cfdea36ba85
柏喜 覃 <1377519410@qq.com>
2020-03-19 21:14:49 +0800

    角色添加未有权限

--- SSMTRAVEL/SSMTRAVEL_dao/src/main/java/cn/qut/dao/IRoleDao.java
+++ SSMTRAVEL/SSMTRAVEL_dao/src/main/java/cn/qut/dao/IRoleDao.java
@@ -30,4 +30,13 @@ public interface IRoleDao {
 
     @Insert("insert into role(roleName,roleDesc) values(#{roleName},#{roleDesc})")
     void save(Role role) throws Exception;
+
+    @Select("select * from role where id =#{roleId}")
+    Role findById(String roleId) throws Exception;
+
+    @Select("select * from permission where id not in (select permissionId from role_permission where roleId=#{roleId})")
+    List<Permission> findOtherPermissions(String roleId) throws Exception;
+
+    @Insert("insert into role_permission(roleId,permissionId) values(#{roleId},#{permissionId})")
+    void addPermissionToRole(@Param("roleId") String roleId, @Param("permissionId") String permissionId);
 }
--- SSMTRAVEL/SSMTRAVEL_service/src/main/java/cn/qut/service/impl/IRoleServiceImpl.java
+++ SSMTRAVEL/SSMTRAVEL_service/src/main/java/cn/qut/service/impl/IRoleServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.qut.service.impl;
 
 import cn.qut.dao.IRoleDao;
+import cn.qut.domain.Permission;
 import cn.qut.domain.Role;
 import cn.qut.service.IRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,22 @@ public class IRoleServiceImpl implements IRoleService {
     @Override
     public void save(Role role) throws Exception {
         roleDao.save(role);
+    }
+
+    @Override
+    public Role findById(String roleId) throws Exception {
+        return roleDao.findById(roleId);
+    }
+
+    @Override
+    public List<Permission> findOtherPermissions(String roleId) throws Exception {
+        return roleDao.findOtherPermissions(roleId);
+    }
+
+    @Override
+    public void addPermissionToRole(String roleId, String[] permissionIds) throws Exception {
+        for(String permissionId:permissionIds){
+            roleDao.addPermissionToRole(roleId,permissionId);
+        }
     }
 }
